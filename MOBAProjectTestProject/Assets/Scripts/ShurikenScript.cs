94e312fb9110673600a5564b8344e895bd8bfb80
Nicholas Doyle <Nic.Doyle15@gmail.com>
2014-03-24 12:51:14 -0700

    spring lab

---
+++ MOBAProjectTestProject/Assets/Scripts/ShurikenScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShurikenScript : MonoBehaviour {
+	public float pullBackTmr;
+	private float elapsedTime;
+	public GameObject player;
+	public Vector3 velocity;
+	public Vector3 acceleration;
+	public float rotationalVelocity;
+	public float rotationalAcceleration;
+
+	// Use this for initialization
+	void Awake(){
+				velocity = Vector3.zero;
+				acceleration = Vector3.zero;
+				rotationalVelocity = 0.0f;
+				rotationalAcceleration = 0.0f;
+		}
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		elapsedTime += Time.deltaTime;
+		velocity += acceleration * Time.deltaTime;
+		transform.Translate(velocity * Time.deltaTime + (0.5f * acceleration * Time.deltaTime * Time.deltaTime), Space.World);
+		acceleration = Vector3.zero; 
+		rotationalVelocity += rotationalAcceleration * Time.deltaTime;
+		this.transform.Rotate(new Vector3(0,rotationalVelocity,0));
+		rotationalAcceleration = 0;
+
+		if (elapsedTime >= pullBackTmr)
+						this.addSpringForce (100.0f, 0.0f);
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		Debug.Log ("hit");
+		if (collider.gameObject == player && !(elapsedTime <= pullBackTmr))
+						Destroy (this.gameObject);
+		}
+
+	public void addSpringForce(float _springStrength, float _springAtRest)
+	{
+		Vector3 difference =  this.transform.position- player.transform.position;
+		float distance = difference.magnitude;
+		acceleration += -_springStrength * difference.normalized;
+	}
+	public void addForce(float _force,Vector3 _direction)
+	{
+		acceleration += _direction.normalized * _force;
+	}
+	public void addRotationalForce(float _force, float _radius)
+	{
+		rotationalAcceleration += _radius * _force;
+	}
+}
---
+++ MOBAProjectTestProject/Assets/Scripts/throwShuriken.cs
@@ -0,0 +1,25 @@
+﻿ using UnityEngine;
+using System.Collections;
+
+public class throwShuriken : MonoBehaviour {
+
+	public float throwStrength =0;
+	public float throwRotationStrength = 0;
+	public GameObject clone;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Return)) 
+		{
+			GameObject shuriken = Instantiate(clone, transform.position, Quaternion.Euler(transform.forward)) as GameObject;
+			shuriken.GetComponent<ShurikenScript>().addForce(throwStrength,transform.forward);
+			shuriken.GetComponent<ShurikenScript>().addRotationalForce(throwRotationStrength,16);
+
+			shuriken.GetComponent<ShurikenScript>().player = this.gameObject;
+		}
+	}
+}
